9da6bb2fa2015f51893a92327816cf5ea9e63f7e
Gwenael Ropert <gwena.code@gmail.com>
2017-11-11 15:52:56 +0200

    Add .imwheelrc with symlink

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -6,12 +6,11 @@ cd ~/
 ln -sfv $script/.zshrc .zshrc
 ln -sfv $script/.bashrc .bashrc
 ln -sfv $script/.sharedrc .sharedrc
-
 ln -sfv $script/.spacemacs .spacemacs
+ln -sfv $script/linux-only/.imwheelrc .imwheelrc
 
 # Check if folder exist - else create first
 chmod go-w /home/gwena/.ghc 
 cd ~/.ghc
 ln -sfv $script/ghci.conf ghci.conf
-
 
