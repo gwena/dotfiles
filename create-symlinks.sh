f9711d93c6c547cc9e947cc6e3df39748bae4a41
Gwenael Ropert <gwena.code@gmail.com>
2017-11-17 14:06:11 +0200

    Add more symlinks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -6,7 +6,16 @@ cd ~/
 ln -sfv $script/.zshrc .zshrc
 ln -sfv $script/.bashrc .bashrc
 ln -sfv $script/.sharedrc .sharedrc
+ln -sfv $script/.profile .profile
 ln -sfv $script/.spacemacs .spacemacs
+ln -sfv $script/.vimrc .vimrc
+ln -sfv $script/.ideavimrc .ideavimrc
+ln -sfv $script/.gitconfig .gitconfig
+ln -sfv $script/.gemrc .gemrc
+ln -sfv $script/.pryrc .pryrc
+ln -sfv $script/.haskeline .haskeline
+
+# Should only do it on Linux machine
 ln -sfv $script/linux-only/.imwheelrc .imwheelrc
 
 # Check if folder exist - else create first
