24b55231553eaabd84f7151dd540a2099a358b75
Gwenael Ropert <gwena.code@gmail.com>
2018-02-22 22:57:00 +0200

    Add eastwood and test-refresh for Lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:repl {:plugins [[lein-pprint "1.2.0"]]}
- :user {:plugins [[jonase/eastwood "0.2.5"]]}
+ :user {:plugins [[jonase/eastwood "0.2.5"]
+                  [com.jakemccrary/lein-test-refresh "0.22.0"]]}
 }
