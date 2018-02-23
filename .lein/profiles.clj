fd5912ddbece88a84c70e8058ff64f697149b08f
Gwenael Ropert <gwena.code@gmail.com>
2018-02-23 23:38:04 +0200

    Add ultra for lein - need red color when tests fail

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:repl {:plugins [[lein-pprint "1.2.0"]]}
  :user {:plugins [[jonase/eastwood "0.2.5"]
+                  [venantius/ultra "0.5.2"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]]}
 }
