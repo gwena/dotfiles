ee03cc65da8a70cdc4f880923b03f691bc6413b6
Gwenael Ropert <gwena.code@gmail.com>
2018-04-05 15:48:20 +0200

    Add nvd plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,5 +4,6 @@
                   [lein-kibit "0.1.6-beta2"]
                   [lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]
+                  [lein-nvd "0.5.1"]
                   [lein-drip "0.1.1-SNAPSHOT"]]}
 }
