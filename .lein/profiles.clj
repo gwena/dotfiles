5d58a308962d8e57cb83b4b2c5d4e120817da199
Gwenael Ropert <gwena.code@gmail.com>
2019-01-13 21:57:45 +0100

    Comment out ultra

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:repl {:plugins [[lein-pprint "1.2.0"]]}
  :user {:plugins [[jonase/eastwood "0.2.5"]
-                  [venantius/ultra "0.5.2"]
+                  #_[venantius/ultra "0.5.2"]
                   [lein-kibit "0.1.6-beta2"]
                   [lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]
