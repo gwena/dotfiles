c6a2adb07e9787a2269c07639a3ed87381463ee9
Gwenael Ropert <gwena.code@gmail.com>
2019-02-19 11:59:22 +0100

    Bump eastwood

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:repl {:plugins [[lein-pprint "1.2.0"]]}
- :user {:plugins [[jonase/eastwood "0.2.5"]
+ :user {:plugins [[jonase/eastwood "0.3.5"]
                   #_[venantius/ultra "0.5.2"]
                   [lein-kibit "0.1.6-beta2"]
                   [lein-ancient "0.6.15"]
