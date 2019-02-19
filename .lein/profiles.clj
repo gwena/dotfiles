758744b8b020526efd0ebbb7d5a66ac1868967b2
Gwenael Ropert <gwena.code@gmail.com>
2019-02-19 12:14:46 +0100

    Bump kibit

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
 {:repl {:plugins [[lein-pprint "1.2.0"]]}
  :user {:plugins [[jonase/eastwood "0.3.5"]
-                  #_[venantius/ultra "0.5.2"]
-                  [lein-kibit "0.1.6-beta2"]
+                  [lein-kibit "0.1.6"]
                   [lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]
+                  #_[venantius/ultra "0.5.2"]
                   [lein-nvd "0.5.1"]
                   [lein-drip "0.1.1-SNAPSHOT"]]}
 }
