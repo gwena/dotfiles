73f1a8b85e08c0fe45c0e29d82d57df60e6cfb6d
Gwenael Ropert <gwena.code@gmail.com>
2019-07-11 11:52:56 +0200

    Add eftest and update dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,8 +2,9 @@
  :user {:plugins [[jonase/eastwood "0.3.5"]
                   [lein-kibit "0.1.6"]
                   [lein-ancient "0.6.15"]
-                  [com.jakemccrary/lein-test-refresh "0.22.0"]
-                  #_[venantius/ultra "0.5.2"]
+                  [com.jakemccrary/lein-test-refresh "0.24.1"]
+                  #_[venantius/ultra "0.6.0"]
+                  [lein-eftest "0.5.8"]
                   [lein-nvd "0.5.1"]
                   [lein-drip "0.1.1-SNAPSHOT"]]}
 }
