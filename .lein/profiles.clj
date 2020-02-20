93bb877246511fade8ce0729ce0894d12392e0e1
Gwenael Ropert <gwena.code@gmail.com>
2020-02-20 17:32:07 +0100

    Add lein-licenses

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,7 @@
                   [lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.24.1"]
                   #_[venantius/ultra "0.6.0"]
+                  [lein-licenses "0.2.2"]
                   [lein-eftest "0.5.8"]
                   [lein-nvd "0.5.1"]
                   [lein-drip "0.1.1-SNAPSHOT"]]}
