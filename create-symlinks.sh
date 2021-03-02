03a6d3959b2d69e7c3067d5945080d7e2f9405c9
Gwenael Ropert <gwena.code@gmail.com>
2021-03-02 21:53:05 +0100

    Update create-symlinks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -7,11 +7,11 @@ ln -sfv $script/.zshrc .zshrc
 ln -sfv $script/.p10k.zsh .p10k.zsh
 ln -sfv $script/.bashrc .bashrc
 ln -sfv $script/.sharedrc .sharedrc
-ln -sfv $script/.spacemacs .spacemacs
+# ln -sfv $script/.spacemacs .spacemacs
 ln -sfv $script/.vimrc .vimrc
 ln -sfv $script/.alacritty.yml .alacritty.yml
 ln -sfv $script/.ideavimrc .ideavimrc
-ln -sfv $script/.tmux.conf .tmux.conf
+# ln -sfv $script/.tmux.conf .tmux.conf
 ln -sfv $script/.gitconfig .gitconfig
 ln -sfv $script/.gitignore_global .gitignore_global
 ln -sfv $script/.ackrc .ackrc
@@ -20,7 +20,7 @@ ln -sfv $script/.inputrc .inputrc
 ln -sfv $script/.haskeline .haskeline
 ln -sfv $script/.gemrc .gemrc
 ln -sfv $script/.pryrc .pryrc
-ln -sfv $script/.joker .joker
+# ln -sfv $script/.joker .joker
 
 ln -sfnv $HOME/Documents/Scripts/.ssh .ssh # Not in dotfiles
 ln -sfnv $HOME/Documents/Scripts/doom.d .doom.d # Own Git Repo
@@ -28,6 +28,7 @@ ln -sfnv $HOME/Documents/Scripts/doom.d .doom.d # Own Git Repo
 cd ~/.config
 rm -rf ranger
 ln -sfnv $script/ranger ranger
+
 ln -sfv $script/redshift.conf redshift.conf
 
 mkdir -p $HOME/.lein
@@ -38,27 +39,27 @@ mkdir -p $HOME/.clojure
 cd $HOME/.clojure
 ln -sfv $script/.clojure/deps.edn deps.edn
 
-mkdir -p $HOME/.jokerd
-cd $HOME/.jokerd
-ln -sfv $script/joker-linter.cljc linter.cljc
+# mkdir -p $HOME/.jokerd
+# cd $HOME/.jokerd
+# ln -sfv $script/joker-linter.cljc linter.cljc
 
-mkdir -p $HOME/.ghc
-chmod go-w $HOME/.ghc
-cd $HOME/.ghc
-ln -sfv $script/ghci.conf ghci.conf
+# Haskell - Not used at the moment
+# mkdir -p $HOME/.ghc
+# chmod go-w $HOME/.ghc
+# cd $HOME/.ghc
+# ln -sfv $script/ghci.conf ghci.conf
+# mkdir -p $HOME/.stack
+# cd $HOME/.stack
+# ln -sfv $script/.stack/config.yaml config.yaml
 
-mkdir -p $HOME/.stack
-cd $HOME/.stack
-ln -sfv $script/.stack/config.yaml config.yaml
-
-sbtConfigFolder=.sbt/0.13/
-sbtPlugins=$sbtConfigFolder/plugins
-mkdir -p $HOME/$sbtPlugins
-cd $HOME/$sbtPlugins
-ln -sfv $script/$sbtPlugins/plugins.sbt plugins.sbt
+# Scala/SBT - Not used at the moment, and next will be 1.x anyway
+# sbtConfigFolder=.sbt/0.13/
+# sbtPlugins=$sbtConfigFolder/plugins
+# mkdir -p $HOME/$sbtPlugins
+# cd $HOME/$sbtPlugins
+# ln -sfv $script/$sbtPlugins/plugins.sbt plugins.sbt
 # cd $HOME/.sbt
 # ln -sfv $script/linux-only-perso/sbt-credentials-nexus .credentials
-
 # Disable for now
 # cd $HOME/$sbtConfigFolder
 # ln -sfv $script/$sbtConfigFolder/global.sbt global.sbt
@@ -72,7 +73,7 @@ case "$(uname -s)" in
         ln -sfv $script/linux-only/.hidden .hidden
         ln -sfv $script/linux-only/.gvimrc .gvimrc
         ln -sfv $script/linux-only/.profile .profile
-        ln -sfv $script/linux-only/urxvt.Xresources .Xresources
+        ln -sfv $script/linux-only/.Xresources .Xresources
         ln -sfnv $script/linux-only/.urxvt .urxvt
         ln -sfnv $script/linux-only/.screenlayout .screenlayout
         ln -sfnv $script/linux-only-perso/.xsessionrc .xsessionrc
@@ -84,12 +85,15 @@ case "$(uname -s)" in
         cd $HOME/.config/gtk-3.0
         ln -sfv $script/linux-only-perso/bookmarks-gtk3-nautilus bookmarks
 
-        mkdir -p $HOME/.config/i3
-        cd $HOME/.config/i3
+        mkdir -p $HOME/.i3
+        cd $HOME/.i3
         ln -sfv $script/linux-only/i3-config config
-        cd $HOME
-        ln -sfv $script/linux-only/i3blocks.conf .i3blocks.conf
 
+        # cd $HOME
+        # ln -sfv $script/linux-only/i3blocks.conf .i3blocks.conf
+
+        cd $HOME/.config
+        ln -sfv $script/linux-only-perso/spotify-tui spotify-tui
 
         ;;
 
