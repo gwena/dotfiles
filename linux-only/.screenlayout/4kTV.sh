0c71df404763c4ae732644c757c43d11e2f139cf
Gwenael Ropert <gwena.code@gmail.com>
2021-09-21 22:12:25 +0200

    Add screenlayouts for xps with/out ultrawide

---
+++ linux-only/.screenlayout/4kTV.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-2 --off --output HDMI-1 --mode 4096x2160 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --off 
---
+++ linux-only/.screenlayout/on-xps-laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
