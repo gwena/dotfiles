2f035de50fc222412a006ec5fc4e8844734be6dd
Gwenael Ropert <gwena.code@gmail.com>
2022-01-08 11:59:09 +0100

    Update rate on ultrawide

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
-{:repl {:plugins [[lein-pprint "1.2.0"]]}
- :user {:plugins [[jonase/eastwood "0.3.5"]
-                  [lein-kibit "0.1.6"]
-                  [lein-ancient "0.6.15"]
+{:repl {:plugins [[lein-pprint "1.3.2"]]}
+ :user {:plugins [[jonase/eastwood "1.0.0"]
+                  [lein-kibit "0.1.8"]
+                  [lein-ancient "1.0.0-RC3"]
                   [com.jakemccrary/lein-test-refresh "0.24.1"]
                   #_[venantius/ultra "0.6.0"]
                   [lein-licenses "0.2.2"]
