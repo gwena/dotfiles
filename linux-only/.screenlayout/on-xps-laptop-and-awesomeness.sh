175655db02ecae05e2b1e9655160673855a541b7
Gwenael Ropert <gwena.code@gmail.com>
2022-01-08 15:31:38 +0100

    Update screenlayout to DP-1

--- linux-only/.screenlayout/on-xps-laptop-and-awesomeness.sh
+++ linux-only/.screenlayout/on-xps-laptop-and-awesomeness.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x1440 --rotate normal --output DP-1 --off --output DP-2 --mode 3440x1440 --pos 152x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x1440 --rotate normal --output DP-2 --off --output DP-1 --mode 3440x1440 --pos 152x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
