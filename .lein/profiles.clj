20c829d20b4aeb1ea147ed151823c43122fc415d
Gwenael Ropert <gwena.code@gmail.com>
2022-02-12 16:09:28 +0100

    Update lein/profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:repl {:plugins [[lein-pprint "1.3.2"]]}
- :user {:plugins [[jonase/eastwood "1.0.0"]
+ :user {:plugins [[jonase/eastwood "1.2.2"]
                   [lein-kibit "0.1.8"]
                   [lein-ancient "1.0.0-RC3"]
                   [com.jakemccrary/lein-test-refresh "0.24.1"]
@@ -7,5 +7,7 @@
                   [lein-licenses "0.2.2"]
                   [lein-eftest "0.5.8"]
                   [lein-nvd "0.5.1"]
-                  [lein-drip "0.1.1-SNAPSHOT"]]}
+                  [lein-drip "0.1.1-SNAPSHOT"]
+                  [lein-cloverage "1.2.2"]]
+        }
 }
