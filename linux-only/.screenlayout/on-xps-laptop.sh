510ed4198ba038a1406a0b69c02b23c672ff4e26
Gwenael Ropert <gwena.code@gmail.com>
2022-02-12 16:08:59 +0100

    Use scale with xrandr for laptop hidpi

--- linux-only/.screenlayout/on-xps-laptop.sh
+++ linux-only/.screenlayout/on-xps-laptop.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
+# scale could also be lower, e.g. 0.675 - to experiment
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.75x0.75 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
