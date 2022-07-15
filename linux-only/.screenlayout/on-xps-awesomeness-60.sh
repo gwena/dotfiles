0fe9ea5b852f01fd4dc62173e46caee3736d88a4
Gwenael Ropert <gwena.code@gmail.com>
2022-07-15 21:28:39 +0200

    Add 60Hz screenlayout and switch to dp1

---
+++ linux-only/.screenlayout/on-xps-awesomeness-60.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --primary --mode 3440x1440 --rate 60 --pos 0x0 --rotate normal --output eDP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
