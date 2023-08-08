88d9cd46c3dce4ece7a47fcc02cf369b34b6ca13
Gwenael Ropert <gwena.code@gmail.com>
2023-08-08 16:05:41 +0200

    Update screenlayouts (scale, switch DP)

--- linux-only/.screenlayout/on-xps-laptop.sh
+++ linux-only/.screenlayout/on-xps-laptop.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-# scale could also be lower, e.g. 0.675 - to experiment
-xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.5x0.5 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
+# scale same value
+# 0.5 - Too big (very comfortable to read, but difficult with code, big spreadsheet)
+# 0.6/0.7 - to try if 0.75 too small
+# 0.75 - Still nice, not too small, good estate
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.75x0.75 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
