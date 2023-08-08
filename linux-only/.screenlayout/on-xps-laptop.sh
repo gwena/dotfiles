6ffe22dc081a7d61b2bbfa068d5891eb49162ed4
Gwenael Ropert <gwena.code@gmail.com>
2023-08-08 18:57:13 +0200

    Use 0.6 for scale on laptop

--- linux-only/.screenlayout/on-xps-laptop.sh
+++ linux-only/.screenlayout/on-xps-laptop.sh
@@ -3,4 +3,4 @@
 # 0.5 - Too big (very comfortable to read, but difficult with code, big spreadsheet)
 # 0.6/0.7 - to try if 0.75 too small
 # 0.75 - Still nice, not too small, good estate
-xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.75x0.75 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.6x0.6 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
