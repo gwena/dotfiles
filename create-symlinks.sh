25e3c1c11fce83a25c8e867efd88c65e3752c311
Gwenael Ropert <gwena.code@gmail.com>
2024-03-08 09:52:28 +0100

    Add dunstrc and symlink

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -98,6 +98,8 @@ case "$(uname -s)" in
 	      ln -sfv $DOTS_PATH/linux-only/i3status-rust i3status-rust
         rm -rf rofi
 	      ln -sfv $DOTS_PATH/linux-only/rofi rofi
+        rm -rf dunst
+	      ln -sfv $DOTS_PATH/linux-only/dunst dunst
         rm -rf spotify-tui
         ln -sfv $SCRIPTS/linux-only-perso/spotify-tui spotify-tui
         rm -rf idasen-controller
