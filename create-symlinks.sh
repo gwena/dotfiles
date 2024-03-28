b390bdd08eeb7a69abbb31fe20255886c3b2bdf7
Gwenael Ropert <gwena.code@gmail.com>
2024-03-28 20:53:21 +0100

    Add basic config, style and symlink (Wofi)

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -105,6 +105,8 @@ case "$(uname -s)" in
         ln -sfv $DOTS_PATH/linux-only/hypr hypr
         rm -rf waybar
         ln -sfv $DOTS_PATH/linux-only/waybar waybar
+        rm -rf wofi 
+        ln -sfv $DOTS_PATH/wofi wofi
         rm -rf spotify-tui
         ln -sfv $SCRIPTS/linux-only-perso/spotify-tui spotify-tui
         rm -rf idasen-controller
