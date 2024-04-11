0b594caae8c33a1d003724b18902b9af2c3e27bf
Gwenael Ropert <gwena.code@gmail.com>
2024-04-11 21:20:44 +0200

    Remove split linux/macos (Sym)

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,7 +5,15 @@ cd "$HOME"
 ln -sfv $DOTS_PATH/.zshrc .zshrc
 ln -sfv $DOTS_PATH/.sharedrc .sharedrc
 ln -sfv $DOTS_PATH/.vimrc .vimrc
+ln -sfv $DOTS_PATH/.profile .profile
+ln -sfv $DOTS_PATH/.xprofile .xprofile
+ln -sfv $DOTS_PATH/.hidden .hidden # Nautilus but also PCManFM
+ln -sfv $DOTS_PATH/.Xresources .Xresources
+ln -sfv $DOTS_PATH/.urxvt .urxvt
 
+rm -rf .screenlayout
+ln -sfv $DOTS_PATH/.screenlayout .screenlayout
+#
 # Not in dotfiles
 ln -sfnv ${SCRIPTS}.ssh .ssh
 
@@ -30,6 +38,29 @@ ln -sfv $DOTS_PATH/redshift.conf redshift.conf
 rm -rf kitty
 ln -sfnv $DOTS_PATH/kitty kitty
 
+rm -rf i3 
+ln -sfv $DOTS_PATH/i3 i3
+rm -rf pcmanfm
+ln -sfv $DOTS_PATH/pcmanfm pcmanfm
+rm -rf i3status-rust
+ln -sfv $DOTS_PATH/i3status-rust i3status-rust
+rm -rf rofi
+ln -sfv $DOTS_PATH/rofi rofi
+rm -rf dunst
+ln -sfv $DOTS_PATH/dunst dunst
+rm -rf hypr
+ln -sfv $DOTS_PATH/hypr hypr
+rm -rf waybar
+ln -sfv $DOTS_PATH/waybar waybar
+rm -rf wofi 
+ln -sfv $DOTS_PATH/wofi wofi
+rm -rf swappy 
+ln -sfv $DOTS_PATH/swappy swappy
+rm -rf spotify-tui
+ln -sfv $SCRIPTS/linux-only-perso/spotify-tui spotify-tui
+rm -rf idasen-controller
+ln -sfv $SCRIPTS/linux-only-perso/idasen-controller idasen-controller
+
 mkdir -p $HOME/.lein
 cd $HOME/.lein
 ln -sfv $DOTS_PATH/.lein/profiles.clj profiles.clj
@@ -39,51 +70,3 @@ cd $HOME/.config/clojure
 ln -sfv $DOTS_PATH/clojure/deps.edn deps.edn
 ln -sfv $DOTS_PATH/clojure/tools tools
 
-cd $HOME
-case "$(uname -s)" in
-    Linux)
-        echo '### Linux only symlinks'
-        # Will be deprecated with Wayland, check /etc/environment and Hyprland config
-        ln -sfv $DOTS_PATH/.profile .profile
-        ln -sfv $DOTS_PATH/.xprofile .xprofile
-
-        ln -sfv $DOTS_PATH/.hidden .hidden # Nautilus but also PCManFM
-        ln -sfv $DOTS_PATH/.Xresources .Xresources
-        ln -sfv $DOTS_PATH/.urxvt .urxvt
-        rm -rf .screenlayout
-        ln -sfv $DOTS_PATH/.screenlayout .screenlayout
-
-        cd $HOME/.config
-        rm -rf i3 
-        ln -sfv $DOTS_PATH/i3 i3
-        rm -rf pcmanfm
-        ln -sfv $DOTS_PATH/pcmanfm pcmanfm
-        rm -rf i3status-rust
-	      ln -sfv $DOTS_PATH/i3status-rust i3status-rust
-        rm -rf rofi
-	      ln -sfv $DOTS_PATH/rofi rofi
-        rm -rf dunst
-	      ln -sfv $DOTS_PATH/dunst dunst
-        rm -rf hypr
-        ln -sfv $DOTS_PATH/hypr hypr
-        rm -rf waybar
-        ln -sfv $DOTS_PATH/waybar waybar
-        rm -rf wofi 
-        ln -sfv $DOTS_PATH/wofi wofi
-        rm -rf swappy 
-        ln -sfv $DOTS_PATH/swappy swappy
-        rm -rf spotify-tui
-        ln -sfv $SCRIPTS/linux-only-perso/spotify-tui spotify-tui
-        rm -rf idasen-controller
-        ln -sfv $SCRIPTS/linux-only-perso/idasen-controller idasen-controller
-        ;;
-
-    Darwin)
-        echo '### MacOS only symlinks'
-        echo 'Will not do anything special'
-        ;;
-
-    *)
-        echo '### Other OS - WEIRD!!!'
-        ;;
-esac
