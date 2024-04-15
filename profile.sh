8da96d599d9fae401a4793b133459ebc2cc7629e
Gwenael Ropert <gwena.code@gmail.com>
2024-04-15 18:01:05 +0200

    Use {} for expansion

--- profile.sh
+++ profile.sh
@@ -1,12 +1,12 @@
 # /etc/profile.d/home.sh export $SCRIPTS and $DOTS_PATH
 #                        source this file and profile-private 
-export ORG="$HOME/Documents/Self/NotesOrgMode"
-export PATH="$PATH:$SCRIPTS"
+export ORG="${HOME}/Documents/Self/NotesOrgMode"
+export PATH="${PATH}:${SCRIPTS}"
 
 # In theory, if $XDG_CONFIG_HOME is either not set or empty, a default equal to $HOME/.config
 # should be used, however not always respected (e.g. Clojure)
-export XDG_CONFIG_HOME="$HOME/.config"
-export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtkrc-2.0"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export GTK2_RC_FILES="${XDG_CONFIG_HOME}/gtkrc-2.0"
 
 export GDK_SCALE=1 # 2 will too big for Thunar
 export GDK_DPI_SCALE=1
@@ -14,7 +14,7 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_QPA_PLATFORMTHEME="qt5ct"
 export BROWSER=/usr/bin/firefox
 
-source "$HOME/.config/broot/launcher/bash/br"
+source "${HOME}/.config/broot/launcher/bash/br"
 
 # Only for X/i3
 if [ $XDG_CURRENT_DESKTOP = "i3" ]; then
