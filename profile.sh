41d7784363e6eab59f2b40b0ee8ca09c3c18774c
Gwenael Ropert <gwena.code@gmail.com>
2024-10-23 17:09:39 +0200

    Tweak profile (profile)

--- profile.sh
+++ profile.sh
@@ -11,11 +11,14 @@ export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
 export XDG_CACHE_HOME="${HOME}/.cache"
 
+# XDG_CONFIG_HOME
 export GTK2_RC_FILES="${XDG_CONFIG_HOME}/gtkrc-2.0"
 export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java -Djavafx.cachedir=${XDG_CACHE_HOME}/openjfx"
 export LEIN_HOME="${XDG_CONFIG_HOME}/lein"
+# XDG XDG_CACHE_HOME
 export GRADLE_USER_HOME="${XDG_CACHE_HOME}/gradle"
 export CUDA_CACHE_PATH="${XDG_CACHE_HOME}/nv"
+# XDG_DATA_HOME
 export _Z_DATA="${XDG_DATA_HOME}/z"
 export CARGO_HOME="${XDG_DATA_HOME}/cargo"
 export DVDCSS_CACHE="${XDG_DATA_HOME}/dvdcss"
@@ -30,17 +33,17 @@ export XCURSOR_PATH=/usr/share/icons:${XDG_DATA_HOME}/icons
 export GDK_SCALE=1 # 2 will too big for Thunar
 export GDK_DPI_SCALE=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
-export QT_QPA_PLATFORMTHEME="qt5ct"
+export QT_QPA_PLATFORMTHEME=qt5ct
 export BROWSER=/usr/bin/firefox
 export LSP_USE_PLISTS=true
 
 source "${SCRIPTS}/fix-desktop-apps.sh"
 
-# Only for X/i3
-if [ $XDG_CURRENT_DESKTOP = "i3" ]; then
+# For i3 only
+if [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
     # Default behavior of xcape: map Control_L to Esc if released
     xcape
-    # Map Super to "non" existing key, will be used by i3wm to map Super alone (simpler, same as Gnome)
+    # Map Super to "non" existing key, used by i3wm to map Super alone (simpler, same as Gnome)
     xcape -t 150 -e 'Super_L=Super_L|Escape'
 fi
 
