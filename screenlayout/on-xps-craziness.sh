f19aa325c7fc19a2d3ef5a799e6c33e057ca9ebe
Gwenael Ropert <gwena.code@gmail.com>
2025-05-10 15:22:01 +0200

    Adapt screenlayout scripts for 5K and Laptop to use wlr-randr (Sway, i3)

--- screenlayout/on-xps-craziness.sh
+++ screenlayout/on-xps-craziness.sh
@@ -6,9 +6,9 @@
 # Automatically check if which DP-1/2/3/4 the monitor is attached to, and use it, with off for all the other outputs
 # Keep it as bash in case bb not yet installed
 rest="--output eDP-1 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off"
+#TODO to simplify, also should not use xrandr for Sway
 dp=$(xrandr | grep " connected " | grep "^DP-" | cut -d " " -f 1)
 dpn=$(echo $dp | cut -d "-" -f 2)
-selection="--output $dp --mode 5120x2160 --rate 72 --pos 0x0 --rotate normal"
 
 other=""
 for i in {1..4}
@@ -18,5 +18,9 @@ do
   fi
 done
 
-xrandr $selection $other $rest
+if [ $XDG_CURRENT_DESKTOP = "sway" ]; then
+  wlr-randr --output $dp --mode 5120x2160 --pos 0,0 --transform normal --output eDP-1 --off
+else
+  xrandr --output $dp --mode 5120x2160 --rate 72 --pos 0x0 --rotate normal $other $rest
+fi
 
--- screenlayout/on-xps-laptop.sh
+++ screenlayout/on-xps-laptop.sh
@@ -3,4 +3,10 @@
 # 0.5 - Too big (very comfortable to read, but difficult with code, big spreadsheet)
 # 0.6/0.7 - to try if 0.75 too small
 # 0.75 - Still nice, not too small, good estate
-xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.6x0.6 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
+
+if [ $XDG_CURRENT_DESKTOP = "sway" ]; then
+  # TODO - need to do something to switch off other monitor
+  wlr-randr --output eDP-1 --on --mode 3840x2400 --pos 0,0 --transform normal
+else
+  xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.6x0.6 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
+fi
